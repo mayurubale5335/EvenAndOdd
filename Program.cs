0cbf4ce7efd1fb8213ec444236ef841c1bb7159f
unknown <mayurubale5335@gmail.com>
2023-04-05 14:26:03 +0530

    [mayur ubale] Adding welcome Message

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenAndOdd
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
